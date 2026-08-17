3bcb81194b7a4bb4886302e095eb13c36c0f5fa4
Irfan <fadillairfanaufa@gmail.com>
2026-08-17 21:21:42 +0700

    feat(note): fix delete repository test

--- src/modul/note/Infrastructures/repositories/NotesRepositoryPostgres_test.go
+++ src/modul/note/Infrastructures/repositories/NotesRepositoryPostgres_test.go
@@ -1,7 +1,6 @@
 package repositories
 
 import (
-	"log"
 	"notes-app/src/commons/database"
 	"notes-app/src/modul/note/Domains/entities"
 	entitiesUser "notes-app/src/modul/user/Domains/entities"
@@ -15,7 +14,6 @@ import (
 )
 
 var repo *NoteRepository
-var id = uuid.New().String()
 
 func TestMain(m *testing.M) {
 	database.ConnectPostgresql(".test.env")
@@ -43,6 +41,7 @@ func TestMain(m *testing.M) {
 
 func Test_CreateNote(t *testing.T) {
 	now := time.Now()
+	id := uuid.New().String()
 
 	note := entities.Note{
 		ID:       id,
@@ -54,79 +53,57 @@ func Test_CreateNote(t *testing.T) {
 	}
 
 	err := repo.CreateNote(note)
-
 	assert.NoError(t, err)
-	log.Println("=============================ini adalah errornya yang pertama========================")
-	log.Println(err)
-	log.Println("=============================ini adalah errornya========================")
 
-	expectedNote := entities.Note{
-		ID: id,
-	}
+	expectedNote := entities.Note{ID: id}
 	err = repo.DB.First(&expectedNote).Error
-
-	
-	log.Println("=============================ini adalah errornya create note========================")
-	log.Println(err)
-	log.Println("=============================ini adalah errornya========================")
 
 	assert.NoError(t, err)
 	assert.Equal(t, note.ID, expectedNote.ID)
 	assert.Equal(t, note.Title, expectedNote.Title)
-	assert.Equal(t, note.Owner, expectedNote.Owner)
+	assert.Equal(t, note.Owner, expectedNote.Owner, gorm.ErrRecordNotFound)
 
-	// repo.db.Exec("DELETE FROM notes")
+	repo.DB.Exec("DELETE FROM notes")
 }
 
-
 func Test_DeleteNote(t *testing.T) {
-	t.Run("id not found", func(t *testing.T) {
-		err := repo.DeleteNoteById("id-123")
+	t.Run("should be error when id not found", func(t *testing.T) {
+		err := repo.DeleteNoteById("note-123")
 
+		assert.Error(t, err)
 		assert.EqualError(t, err, "id tidak ditemukan")
 	})
 
-	t.Run("delete success", func(t *testing.T) {
-		id2 := uuid.New().String()
+	t.Run("delete note success", func(t *testing.T) {
 		now := time.Now()
-	
+		id := uuid.New().String()
+
 		note := entities.Note{
-			ID:       id2,
+			ID:       id,
 			Title:    "test database",
 			Content:  "test content database",
 			CreateAt: now,
 			UpdateAt: now,
 			Owner:    "user-123",
 		}
-	
+
 		err := repo.CreateNote(note)
-	
 		assert.NoError(t, err)
-		log.Println("=============================ini adalah errornya yang pertama delete note========================")
-		log.Println(err)
-		log.Println("=============================ini adalah errornya========================")
 
-		
-		var existNote entities.Note
-		err = repo.DB.First(&existNote, "id = ?", id2).Error
-		if err != nil {
-			log.Println("=============================================")
-			log.Println("Data note tidak ditemukan")
-			log.Println("=============================================")
-		} else {
-			log.Println("=============================================")
-			log.Println("Data note ditemukan")
-			log.Println(existNote.ID)
-			log.Println("=============================================")
-		}
+		exisistNote := entities.Note{ID: id}
 
-		err = repo.DeleteNoteById(existNote.ID)
+		err = repo.DB.First(&exisistNote).Error
+		assert.NoError(t, err)
+		assert.Equal(t, note.Title, exisistNote.Title)
+		assert.Equal(t, note.Content, exisistNote.Content)
+
+		err = repo.DeleteNoteById(id)
 
 		assert.NoError(t, err)
 
-		var deletedNote entities.Note
-		err = repo.DB.First(&deletedNote, "id = ?", id).Error
+		newExisistNote := entities.Note{ID: id}
 
+		err = repo.DB.First(&newExisistNote).Error
 		assert.Error(t, err)
 		assert.ErrorIs(t, err, gorm.ErrRecordNotFound)
 	})
